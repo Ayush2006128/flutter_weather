58d6a7cad708d29b93bbb007cb1604288ff5bd77
”Ayush <ayush.muley1907@outlook.com>
2024-09-16 04:58:01 +0000

    minor fixes

--- lib/weather/widgets/weather_empty.dart
+++ lib/weather/widgets/weather_empty.dart
@@ -13,7 +13,7 @@ class WeatherEmpty extends StatelessWidget {
         SizedBox(
           width: 64,
           height: 64,
-          child: Lottie.asset('assets/rainy.json'),
+          child: Lottie.asset('assets/hello.json'),
         ),
         Text(
           'Please Select a City!',
