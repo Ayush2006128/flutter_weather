014794b8842aeee6f634edcf62a8139b52f73df5
Ayush2006128 <74146837+Ayush2006128@users.noreply.github.com>
2024-09-28 16:20:03 +0530

    home_widget.dart

--- android/app/src/main/java/com/example/flutter_weather/HomeWidget.kt
+++ android/app/src/main/java/com/example/flutter_weather/HomeWidget.kt
@@ -5,6 +5,8 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
 
+import es.antonborri.home_widget.HomeWidgetPlugin
+
 /**
  * Implementation of App Widget functionality.
  */
@@ -16,7 +18,10 @@ class HomeWidget : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            val widgetData = HomeWdgetPlugin.getData(context)
+            val views = RemoteViews(context.packageName, R.layout.home_widget).apply {
+                setTextViewText(R.id.appwidget_text, widgetData)
+            }
         }
     }
 
